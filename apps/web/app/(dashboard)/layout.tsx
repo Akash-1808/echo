892e1243109cddbdd859520d9f09d3013a7e6ab4
Akash-1808 <akashch1808@gmail.com>
2025-08-29 11:37:41 +0530

    07:dashboard-layout

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@
 import { AuthGuard } from '@/modules/auth/ui/components/auth-guard'
 import { OrganizationGuard } from '@/modules/auth/ui/components/organization-guard'
 import { AuthLayout } from '@/modules/auth/ui/layouts/auth-layout'
+import { DashboardLayout } from '@/modules/dashboard/ui/layouts/dashboard-layout'
 import { useOrganization } from '@clerk/nextjs'
 import React from 'react'
 
@@ -10,9 +11,9 @@ const Layout = ({children}: {children: React.ReactNode}) => {
   
 
   return (
-    <AuthGuard>
-      <OrganizationGuard>{children}</OrganizationGuard>
-    </AuthGuard>
+    <DashboardLayout>
+      {children}
+    </DashboardLayout>
   )
 }
 
